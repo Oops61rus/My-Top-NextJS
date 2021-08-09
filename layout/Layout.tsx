8f9a73ba90b99f25a2eeb335d1f95f4871a4986b
Oops61rus <antodeni@rambler.ru>
2021-08-09 19:06:52 +0300

    my_top/14: add Footer.tsx add date format package fix styles in Layout.tsx

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,19 +1,18 @@
 import React, { Component, FC } from 'react';
 import { ILayoutProps } from './Layout.props';
 import { Footer, Header, Sidebar } from './';
+import styles from './Layout.module.sass';
 
 const Layout = ({ children, ...props }: ILayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>
-          {children}
-        </div>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
+        {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
