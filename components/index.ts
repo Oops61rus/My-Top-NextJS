213054f655f370b4679c6ab3792d9bb792b46c30
Oops61rus <antodeni@rambler.ru>
2021-08-09 16:06:06 +0300

    my_top/11: create Tags component fix names in variables.sass

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,11 @@
 import Htag from './Htag/Htag';
 import Button from './Buttons/Button';
 import Paragraph from './Paragraph/Paragraph';
+import Tag from './Tag/Tag';
 
 export {
   Htag,
   Button,
   Paragraph,
+  Tag,
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from '../components';
+import { Button, Htag, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,11 @@ export default function Home(): JSX.Element {
       <Htag tag={'h1'}>Text</Htag>
       <Button appearance='primary'>Button</Button>
       <Button appearance='ghost' arrow={'down'}>Button</Button>
+      <Tag size='s'>hello</Tag>
+      <Tag size='m' color='green'>hello</Tag>
+      <Tag size='m'>hello</Tag>
+      <Tag size='s' color='red' href='https://google.com'>hello</Tag>
+      <Tag size='s' color='primary'>hello</Tag>
     </>
   );
 }
