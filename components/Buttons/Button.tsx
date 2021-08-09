a84ef665bb6cf31cf75221c259073de1dd21488d
Oops61rus <antodeni@rambler.ru>
2021-08-09 15:31:42 +0300

    my_top/9: add arrow.svg add ArrowIcon to Button.tsx fix next.config.js

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import cn from 'classnames';
 import { IButtonProps } from './Button.props';
+import { ArrowIcon } from '../Icons';
 import styles from './Button.module.sass';
 
-const Button = ({ children, appearance, className, ...props }: IButtonProps): JSX.Element => {
+const Button = ({ children, arrow = 'none', appearance, className, ...props }: IButtonProps): JSX.Element => {
   return (
     <button
       className={cn(styles.button, className, {
@@ -13,6 +14,10 @@ const Button = ({ children, appearance, className, ...props }: IButtonProps): JS
       {...props}
     >
       {children}
+      {arrow !== 'none' && <ArrowIcon className={cn(styles.arrow, {
+        [styles.down]: arrow === 'down',
+        [styles.right]: arrow === 'right'
+      })}/>}
     </button>
   );
 };
