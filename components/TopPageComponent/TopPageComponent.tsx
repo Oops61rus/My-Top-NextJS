0dfef430a421f78af94d07bc0e233d372e4828c7
Oops61rus <antodeni@rambler.ru>
2021-08-12 12:59:38 +0300

    my_top/25: create TopPageComponent.tsx

--- components/TopPageComponent/TopPageComponent.tsx
+++ components/TopPageComponent/TopPageComponent.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { ITopPageComponentProps } from './TopPageComponent.props';
+import { HhCard, Htag, Tag } from '../index';
+import styles from './TopPageComponent.module.sass';
+import { TopLevelCategory } from '../../interfaces/page.interface';
 
 const TopPageComponent = ({ page, products, firstCategory }: ITopPageComponentProps): JSX.Element => {
   return (
-    <div>
-      {products?.length}
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag={'h1'}>{page.title}</Htag>
+        {products && <Tag color={'grey'} size={'m'}>{products.length}</Tag>}
+        <span>Сортировка</span>
+      </div>
+      <div>
+        {products && products.map(product => <div key={product._id}>{product.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag={'h2'}>Вакансии - {page.category}</Htag>
+        <Tag color={'red'} size={'m'}>hh.ru</Tag>
+      </div>
+      {firstCategory === TopLevelCategory.Courses && <HhCard {...page.hh} />}
     </div>
   );
 };
