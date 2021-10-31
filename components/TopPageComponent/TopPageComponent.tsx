b5fecd2e9dd742d773e88bead877608601fa5041
Oops61rus <fit_Denisov>
2021-11-01 01:23:29 +0300

    my_top/57: - add invalid fields for screen readers

--- components/TopPageComponent/TopPageComponent.tsx
+++ components/TopPageComponent/TopPageComponent.tsx
@@ -23,8 +23,8 @@ const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentPro
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
-        <Htag tag={'h1'}>{page?.title}</Htag>
-        {products && <Tag color={'grey'} size={'m'}>{products.length}</Tag>}
+        <Htag tag='h1'>{page?.title}</Htag>
+        {products && <Tag color='grey' size='m' aria-label={products.length + 'элементов'}>{products.length}</Tag>}
         <Sort
           sort={sort}
           setSort={setSort}
@@ -37,21 +37,21 @@ const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentPro
         ))}
       </div>
       <div className={styles.hhTitle}>
-        <Htag tag={'h2'}>Вакансии - {page.category}</Htag>
-        <Tag color={'red'} size={'m'}>hh.ru</Tag>
+        <Htag tag='h2'>Вакансии - {page.category}</Htag>
+        <Tag color='red' size='m'>hh.ru</Tag>
       </div>
       {firstCategory === TopLevelCategory.Courses && page.hh && <HhCard {...page.hh} />}
       {page.advantages &&
       page.advantages.length > 0 && (
         <>
-          <Htag tag={'h2'}>Преимущества</Htag>
+          <Htag tag='h2'>Преимущества</Htag>
           <Advantages advantages={page.advantages} />
         </>
       )}
       {
         page?.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />
       }
-      <Htag tag={'h2'}>Получаемые навыки</Htag>
+      <Htag tag='h2'>Получаемые навыки</Htag>
       {page.tags.map(tag => <Tag color='primary' className={styles.getSkills} key={tag}>{tag}</Tag>)}
     </div>
   );
