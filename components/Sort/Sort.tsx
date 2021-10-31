e062491009cbc03d838bc04fff21f137df27a2d0
Oops61rus <fit_Denisov>
2021-11-01 00:23:35 +0300

    my_top/57: - fix accessibility: fix sort

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -6,22 +6,22 @@ import styles from './Sort.module.sass';
 
 const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => (
   <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
           [styles.active]: sort === SortEnum.Rating,
         })}
       >
         <SortIcon className={styles.sort__icon} /> По рейтингу
-      </span>
-    <span
+      </button>
+    <button
       onClick={() => setSort(SortEnum.Price)}
       className={cn({
         [styles.active]: sort === SortEnum.Price,
       })}
     >
         <SortIcon className={styles.sort__icon} /> По цене
-      </span>
+      </button>
   </div>
 );
 
